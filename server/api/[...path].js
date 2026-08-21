19290fc185d548736c2236efc8ded204be889ef7
Muhannad Salah <smohanad540@gmail.com>
2026-08-21 03:23:36 +0300

    debug vercel api routing

--- server/api/[...path].js
+++ server/api/[...path].js
@@ -2,6 +2,12 @@ import app from '../src/app.js';
 import { connectDB } from '../src/config/db.js';
 
 export default async function handler(req, res) {
+  console.log('VERCEL API HIT:', {
+    method: req.method,
+    url: req.url,
+    originalUrl: req.originalUrl,
+  });
+
   try {
     await connectDB();
     return app(req, res);
